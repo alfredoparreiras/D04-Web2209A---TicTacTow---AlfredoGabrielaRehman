25409d78911521e388de74ec3794c629b51d3f80
gfrancoa <gfrancoa@outlook.com>
2022-12-22 20:50:30 -0500

    adding initial socket boilerplate that needs to be adapted and adjustments in views

--- TicTacToe/src/controllers/MainMenuController.java
+++ TicTacToe/src/controllers/MainMenuController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import sockets.client.Client;
+import sockets.server.Server;
 import utility.swing.windows.Window;
 import views.GameView;
 import views.MainMenu;
@@ -12,7 +14,7 @@ public class MainMenuController {
     private final MainMenu view;
     private final GameView gameView;
 
-    private final java.awt.Window buttonsWindow;
+    private final Window buttonsWindow;
 
     public MainMenuController(MainMenu view, GameView gameView, Window buttonsWindow){
         this.view= Objects.requireNonNull(view);
@@ -23,18 +25,34 @@ public class MainMenuController {
         view.addJoinGameListener(e->onJoinGameClicked(e));
     }
 
-    private void onLocalGameClicked(ActionEvent e){
-        utility.swing.windows.Window window = new Window("Board", gameView, true); // Create window and set content pane
+    private void showBoard(){
+        //TODO
+        // Create window and set content pane
+        Window window = new Window("Board", gameView, true);
         window.setVisible(true); // Display window
         buttonsWindow.setVisible(false);
+    }
 
+    private void onLocalGameClicked(ActionEvent e){
+        showBoard();
     }
 
     private void onHostGameClicked(ActionEvent e){
-        System.out.println("Host game clicked");
-    } //TODO
+        //TODO
+        // send the gameview and controller here
+        Server server = new Server(7777);
+        // TODO: Create thread with server::start as the Runnable (daemon)
+        // TODO: Start thread
+        server.start();
+        showBoard();
+    }
 
     private void onJoinGameClicked(ActionEvent e){
-        System.out.println("Join game clicked");
-    } //TODO
+        //TODO
+        // replace IP. send the gameview and controller here
+
+        Client client = new Client("localhost", 7777);
+        client.start();
+        showBoard();
+    }
 }
